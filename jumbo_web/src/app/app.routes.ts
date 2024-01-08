68245f9a99e1a46449b079bf308f76720e0a1608
Dany Paredes <danywalls4@gmail.com>
2024-01-08 18:34:26 +0100

    feat: :building_construction: add new routing path

--- jumbo_web/src/app/app.routes.ts
+++ jumbo_web/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CardsComponent} from "./feature/cards/cards.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: CardsComponent
+  }
+];
